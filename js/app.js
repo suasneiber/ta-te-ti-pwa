d438cdb4443a456c41d5a4dfedc9e8bd26c36bc8
suasneiber <57971818+suasneiber@users.noreply.github.com>
2019-11-28 22:49:29 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 if('serviceWorker'in navigator){
-    navigator.serviceWorker.register('/serviceworker.js')
+    navigator.serviceWorker.register('../serviceworker.js')
     .then((reg) =>console.log(" service Worker Registrado",reg))
     .catch((err) =>console.log ("Service Worker No registrado. :(",err));
-
+    console.log("hola");
 }
