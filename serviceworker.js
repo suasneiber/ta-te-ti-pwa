6bd901a69d6fc640a712fcc6202f647d60e24317
suasneiber <57971818+suasneiber@users.noreply.github.com>
2019-12-03 21:42:07 -0300

    activar sw

--- serviceworker.js
+++ serviceworker.js
@@ -2,3 +2,7 @@
 self.addEventListener("install",evt =>{
     console.log("el service Worker se Instaló");
 });
+
+///activar ek service worker
+self.addEventListener("activate", evt=>{
+    console.log("El service worker se activó");});
